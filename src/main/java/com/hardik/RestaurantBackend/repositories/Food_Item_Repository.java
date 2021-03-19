9c64c83a134fe46021a13e68095970824abc2d54
hardi <hardikvariya98@gmail.com>
2021-03-19 12:24:16 +0530

    form data coming from front-end saved to db

--- src/main/java/com/hardik/RestaurantBackend/repositories/Food_Item_Repository.java
+++ src/main/java/com/hardik/RestaurantBackend/repositories/Food_Item_Repository.java
@@ -3,6 +3,11 @@ package com.hardik.RestaurantBackend.repositories;
 import com.hardik.RestaurantBackend.models.FoodItems;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface Food_Item_Repository extends JpaRepository<FoodItems, Long> {
+    void deleteFoodItemById(Long id);
+
+    Optional<FoodItems> findFoodItemById(Long id);
 }
